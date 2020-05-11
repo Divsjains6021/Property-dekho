4ef3707a627f3320f399be6658fd05311006ee70
Divsjains6021 <divsjains6021@gmail.com>
2020-05-11 22:34:20 +0530

    adding necessary files to Git

---
+++ src/main/java/com/propertydekho/PropertyDetails.java
@@ -0,0 +1,11 @@
+package com.propertydekho;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class PropertyDetails {
+    @JsonProperty("prop_id") private String propID;
+    @JsonProperty("prop_name") private String propName;
+    @JsonProperty("prop_price") private double propPrice;
+}
