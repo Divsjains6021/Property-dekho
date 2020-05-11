553ee6737d390eaac4eb63bd74a26489db27aa63
Divsjains6021 <divsjains6021@gmail.com>
2020-05-11 22:37:53 +0530

    adding necessary files to Git

---
+++ src/main/java/com/propertydekho/BuilderDetails.java
@@ -0,0 +1,4 @@
+package com.propertydekho;
+
+public class BuilderDetails {
+}
---
+++ src/test/java/com/propertydekho/PropertyDekhoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.propertydekho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PropertyDekhoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
